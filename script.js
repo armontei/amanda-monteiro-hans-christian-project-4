202fd7f91e67ce5845e6dbb9cb95c998c13cef25
Amanda Monteiro <a3monteiro@gmail.com>
2020-11-19 20:46:33 -0500

    filter buttons and results display

--- script.js
+++ script.js
@@ -1,92 +1,77 @@
 // Create app namespace to hold all methods
 const app = {};
 
-// Get input from user.
-// app.collectInfo = function(){ 
+app.displayRecipes = function (dishes) {
 
-// };
+    // Display user input results on page.
+    dishes.forEach(function (dish) {
+        const displayOnPage = `
+        <ul class="recipesContainer">
+            <li>
+                <h3>${dish.strMeal}</h3>
+                <img src="${dish.strMealThumb}" alt="${dish.strMeal}">
+                <a href="${dish.strSource}" target="_blank">Full Recipe</a>
+                <a href="${dish.strYoutube}" target="_blank">YouTube</a>
+                <p>Country of origin: ${dish.strArea}</p>
+            </li>
+        </ul>
+        `
+        $(`.recipes`).append(displayOnPage);
 
-// Prevent default function of the form.
+        // adding each country to a button that the user can filter recipes by
+        $(`.recipeFilter`).append(`<li><button>${dish.strArea}</button></li>`);
+
+        // removing duplicate buttons, inspo code from https://stackoverflow.com/questions/14940237/faster-jquery-code-for-duplicate-list-item-removal
+        let duplicate = {};
+
+        $(`button`).each(function () {
+            let buttonText = $(this).text();
+            if (duplicate[buttonText])
+                $(this).remove();
+            else
+                duplicate[buttonText] = true;
+        });
+        return false;
+    })
+}
 
 // Make AJAX request with user inputted data
-
-app.getRecipes = function  (food ) {
+app.getRecipes = function (food) {
     $.ajax({
         url: `https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`,
         method: 'GET',
-        dataType: 'json'
-    }).then(function  (result)  {
+        dataType: 'json',
+    }).then(function (result) {
 
-        
+        //Store results in a variable for reuse. 
+        const results = result.meals;
 
-    const results = result.meals;
-    console.log(results);
-        // console.log(result.meals);
-        for (let i = 0; i < results.length; i +=1){
-            console.log(results[i].strMeal, results[i].strMealThumb, results[i].strYoutube, results[i].strSource, results[i].strArea);
-            
-        }
-
-        $('.recipes').append(`<h3>${results[0].strMeal}</h3>
-            <img src="${results[0].strMealThumb}" alt="${results[0].strMeal}">
-            <p>Origin: ${results[0].strArea}</p>
-            <a href="${results[0].strSource}">Full recipe</a>
-            <a href="${results[0].strYoutube}">Recipe video</a>`);
-
-        // app.displayRecipes(result);
+        app.displayRecipes(results);
     })
-    
-    
-};  
+};
 
 // get input from user
-app.formSubmit = function(){
-    $(`form`).on(`submit`, function(e){
+app.formSubmit = function () {
+    $(`form`).on(`submit`, function (e) {
+
+        // Prevent default function of the form.
         e.preventDefault();
-        
+
         const userInput = $(`input:text`).val();
-        // console.log(userInput);
 
-        app.getRecipes(userInput)
+        // Clear results before displaying new ones.
+        $('.recipes').empty();
+        $('.recipeFilter').empty();
 
-        // $('.recipes').empty();
-        
-        // Maybe use forEach();
-
+        app.getRecipes(userInput);
     })
 }
 
-app.displayRecipes = function(meals) {
-
-    meals.forEach(meal => {
-       
-    })
-    
-}
-
-
-
-// Lookup a single random meal:
-// https://www.themealdb.com/api/json/v1/1/random.php
-
-// if === null;
-
-// dishName = meals.strMeal
-// images = result['strMealThumb']
-// tags = result['strTags']
-// youtube link = result['strYoutube']
-// website link = result['strSource']
-// origin = result['strArea']
-
-// Display data on the page
-// app.displayRecipes = function (recipes) {
-
-// };
+// Filter by country origin
 
 // Initialize app
-app.init = function () { 
+app.init = function () {
     app.formSubmit();
-    
 };
 
 // document ready
